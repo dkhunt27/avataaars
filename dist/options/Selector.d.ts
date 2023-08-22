a76a16cb732742e6a475ac62346c2c18ef9f97f0
JacobDel <jacob.delabie@hotmail.com>
2023-08-22 15:51:15 +0200

    build

--- dist/options/Selector.d.ts
+++ dist/options/Selector.d.ts
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import Option from './Option';
 export interface Props {
     option: Option;
--- dist/options/Selector.js
+++ dist/options/Selector.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = require("react");
+var React = require("react");
 var avatarContext_1 = require("./avatarContext");
 function getComponentOptionValue(component) {
     var optionValue = component.optionValue;
@@ -10,11 +10,11 @@ function getComponentOptionValue(component) {
     return optionValue;
 }
 function Selector(props) {
-    var avatarContext = (0, react_1.useContext)(avatarContext_1.AvatarContext);
+    var avatarContext = React.useContext(avatarContext_1.AvatarContext);
     function getSelectedOption() {
         var selectedOptionType = avatarContext[props.option.key];
         var result;
-        react_1.default.Children.forEach(props.children, function (child) {
+        React.Children.forEach(props.children, function (child) {
             if (getComponentOptionValue(child.type) === selectedOptionType) {
                 result = child;
             }
@@ -26,7 +26,7 @@ function Selector(props) {
             return result;
         }
     }
-    var selectedOption = (0, react_1.useMemo)(function () { return getSelectedOption(); }, [avatarContext]);
+    var selectedOption = React.useMemo(function () { return getSelectedOption(); }, [avatarContext]);
     return selectedOption;
 }
 exports.default = Selector;
